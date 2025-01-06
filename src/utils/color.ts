800d3a2c460869347847f32b55f50bc8186e8248
Martin <mpatino117@gmail.com>
2025-01-05 23:04:18 -0700

    feat: refactor greet and bye commands to use custom color utility and enhance output formatting

---
+++ src/utils/color.ts
@@ -0,0 +1,6 @@
+import { colors } from "https://deno.land/x/cliffy@v0.25.5/ansi/colors.ts";
+
+export const error = colors.bold.red;
+export const warn = colors.bold.yellow;
+export const info = colors.bold.blue;
+export const success = colors.bold.green;
